e9b50874fc1a201c4034ca9dd385c39fd9dbadd4
Itsuki06 <shyorn0604@gmail.com>
2026-09-08 00:10:00 +0700

    Add React entry point

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@fontsource-variable/manrope'
+import '@fontsource-variable/lora'
+import '@fontsource-variable/lora/wght-italic.css'
+import App from './App'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
